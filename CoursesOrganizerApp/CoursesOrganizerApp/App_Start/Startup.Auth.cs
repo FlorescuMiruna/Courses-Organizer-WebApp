25dadde7035e0f33a740690f3bacd37a43ccf7df
AnaMariaAvram <62396340+AnaMariaAvram@users.noreply.github.com>
2021-06-01 23:08:40 +0300

    Added context for user manager

--- CoursesOrganizerApp/CoursesOrganizerApp/App_Start/Startup.Auth.cs
+++ CoursesOrganizerApp/CoursesOrganizerApp/App_Start/Startup.Auth.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -18,6 +18,7 @@ namespace CoursesOrganizerApp
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
