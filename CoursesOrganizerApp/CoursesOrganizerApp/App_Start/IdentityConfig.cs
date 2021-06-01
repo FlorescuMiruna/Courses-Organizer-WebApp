e700e6c171cc6acc40719fd13df49518f456c4b7
AnaMariaAvram <62396340+AnaMariaAvram@users.noreply.github.com>
2021-06-01 23:10:04 +0300

    Update IdentityConfig.cs
    Config for role manager

--- CoursesOrganizerApp/CoursesOrganizerApp/App_Start/IdentityConfig.cs
+++ CoursesOrganizerApp/CoursesOrganizerApp/App_Start/IdentityConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -40,7 +40,7 @@ namespace CoursesOrganizerApp
         {
         }
 
-        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
             var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
             // Configure validation logic for usernames
@@ -81,10 +81,25 @@ namespace CoursesOrganizerApp
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
             {
-                manager.UserTokenProvider = 
+                manager.UserTokenProvider =
                     new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
             }
             return manager;
+        }
+    }
+    public class ApplicationRoleManager : RoleManager<IdentityRole>
+    {
+        public ApplicationRoleManager(IRoleStore<IdentityRole, string> store) :
+        base(store)
+        {
+        }
+        public static ApplicationRoleManager
+        Create(IdentityFactoryOptions<ApplicationRoleManager> options,
+        IOwinContext context)
+        {
+            var roleStore = new
+            RoleStore<IdentityRole>(context.Get<ApplicationDbContext>());
+            return new ApplicationRoleManager(roleStore);
         }
     }
 
