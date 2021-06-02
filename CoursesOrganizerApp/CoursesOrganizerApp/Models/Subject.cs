d88cf0e20577a56a76a1497bec910b60b825f4b4
stefanst77 <46823784+stefanst77@users.noreply.github.com>
2021-06-02 18:19:48 +0300

    Added subject Model

---
+++ CoursesOrganizerApp/CoursesOrganizerApp/Models/Subject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoursesOrganizerApp.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int SubjectId { get; set; }
+        [Required]
+        public string SubjectName { get; set; }
+        public string TeacherName { get; set; }
+        public string Description { get; set; }
+
+
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
